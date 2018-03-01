cd116f02637b029e7d4b2f9cbbf11e8822b979e0
shiyou.xin <shiyou.xin@owitho.com>
2018-03-01 14:36:33 +0800

    add project test-kafka2es

---
+++ test-java/src/main/java/com/hundun/java/thread/multi/hashmap/Main.java
@@ -0,0 +1,15 @@
+package com.hundun.java.thread.multi.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<String, String> map = new HashMap<String, String>();
+
+	}
+
+}
---
+++ test-kafka2es/src/main/java/com/hundun/kafka2es/Main.java
@@ -0,0 +1,12 @@
+package com.hundun.kafka2es;
+
+/**
+ * @DESC extract Kafka data to ES
+ * @author xinshiyou
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
